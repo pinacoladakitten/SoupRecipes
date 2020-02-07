ed2ac4546114302aa786e173333acb3d347a7e00
Anthony Jimerson <53334211+pinacoladakitten@users.noreply.github.com>
2020-02-06 16:17:58 -0800

    Add files via upload

---
+++ Scripts/script.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO.Ports;
+
+public class script : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        SerialPort port1 = new SerialPort("COM0", 9600);
+        SerialPort bluetooth = new SerialPort("COM1", 38400);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(port1.Read())
+        {
+            Debug.Log("Data: " + port1.Read());
+        }
+        
+    }
+}
